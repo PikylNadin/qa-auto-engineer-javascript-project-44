1cd86cc29333b0dcc39cccd709a0472214de10d1
PikylNadin <pikylnadin@yandex.ru>
2025-09-18 10:32:38 +0300

    Устранение ошибки

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,13 @@
 import readlineSync from 'readline-sync';
 
 
-export default () => {
+const greetUser = () => {
   console.log("Welcome to the Brain Games!");
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
 };
+
+export default greetUser;
 
 
