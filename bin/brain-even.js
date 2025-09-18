9f4e752d111449de2bcb4482f47878471deaec46
PikylNadin <pikylnadin@yandex.ru>
2025-09-18 12:24:33 +0300

    «Корректировка»

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 import playBrainEven from '../src/games/even.js';
 
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 import playBrainPrime from '../src/games/prime.js';
 
